27dbc35b5697e2f7889cf003088745f876d6f640
Olga Mirensky <5200844+olga-mir@users.noreply.github.com>
2023-10-30 21:52:44 +1100

    aws-eks: cleanup fixes

--- aws/eks-cleanup.sh
+++ aws/eks-cleanup.sh
@@ -10,9 +10,11 @@ eksctl delete cluster --name=$cluster_name
 vpc_id=$(aws cloudformation describe-stacks --stack-name $vpc_stack --query 'Stacks[0].Outputs[?OutputKey==`VpcId`].OutputValue' --output=text)
 
 # in next iteratation can use additional tags to filter only subnets that are not part of stacks
+sleep 30
 subnets=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpc_id" --filters "Name=vpc-id,Values=$vpc_id" --query 'Subnets[*].{SubnetId: SubnetId}' --output text)
 for s in ${subnets[@]}; do
   aws ec2 delete-subnet --subnet-id $s
 done
 
-aws cloudformation delete-stack --stack-name my-eks-custom-networking-vpc
+aws cloudformation delete-stack --stack-name $vpc_stack
+aws cloudformation wait stack-delete-complete --stack-name $vpc_stack
